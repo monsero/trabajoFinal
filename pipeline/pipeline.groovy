4dcd20078f0944dfaef22503ee1b47e31080dc4a
monsero <monselu.060219@gmail.com>
2020-09-19 17:30:55 -0500

    Create pipeline.groovy

---
+++ pipeline/pipeline.groovy
@@ -0,0 +1,65 @@
+pipeline {
+
+    environment {
+        BASE_GIT_URL = 'https://github.com/monsero'
+        APP_REPO_URL = "${env.BASE_GIT_URL}/${nombre_repo}.git"
+        INFRA_REPO_URL = "${env.BASE_GIT_URL}/Infraestructura.git"
+        DOCKER_IMAGE = "sepulveda/${nombre_repo}"
+        DEPLOY_FOLDER = "deploy/kubernete/${nombre_repo}"
+    }
+
+    agent any
+    stages {
+        stage("Checkout app-code") {
+            steps {
+            //se esta crando una carpeta....
+               dir('app') {
+                    git url:"${env.APP_REPO_URL}" , branch: "${version}"
+                } 
+            }
+        }
+         
+         stage("Checkout deploy-code") {
+            steps {
+               dir('deploy') {
+                    git url:"${env.INFRA_REPO_URL}" , branch: "master"
+                } 
+            }
+        }
+        
+         stage("Build image") {
+            steps {
+                dir('app') {
+                    script {
+                        dockerImage = docker.build("${env.DOCKER_IMAGE}:${tag}")
+                    }
+                }
+            }
+        }
+        
+        stage("Push image") {
+            steps {
+                script {
+                    docker.withRegistry('', 'monicadocker') {
+                    dockerImage.push()
+                    }
+                }
+            }
+        }
+        
+        stage('Deploy') {
+            steps{
+                sh "sed -i 's:DOCKER_IMAGE:${env.DOCKER_IMAGE}:g' ${DEPLOY_FOLDER}/deployment.yaml"
+                sh "sed -i 's:TAG:${tag}:g' ${DEPLOY_FOLDER}/deployment.yaml"
+                
+                step([$class: 'KubernetesEngineBuilder', 
+                        projectId: "wired-effort-288603",
+                        clusterName: " cluster-test",
+                        zone: "us-central1-c",
+                        manifestPattern: "${DEPLOY_FOLDER}/",
+                        credentialsId: "My First Project",
+                        verifyDeployments: true])
+            }
+        }
+    }
+}
